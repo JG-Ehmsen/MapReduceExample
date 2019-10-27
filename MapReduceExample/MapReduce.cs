b35ac136ee8e38dabc530847d48f8eb49b8d1c54
Jeppe Ehmsen <jeppeehmsen@gmail.com>
2019-10-27 15:17:00 +0100

    Updated to include parallel.

--- MapReduceExample/MapReduce.cs
+++ MapReduceExample/MapReduce.cs
@@ -1,43 +1,111 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace MapReduceExample
 {
     public class MapReduce
     {
+        const string inputFile = "Input.txt";
         public MapReduce()
         {
-            var fileStream = new FileStream("Input.txt", FileMode.Open);
+            while (true)
+            {
+                Console.Write("Running parallel...");
+                Stopwatch sw = Stopwatch.StartNew();
+                var dictPara = RunParallel(inputFile);
+                sw.Stop();
+                Console.WriteLine(" Done.");
+                Console.WriteLine("Parallel time: {0:F6}", (sw.ElapsedMilliseconds / 1000.0));
+                Console.Write("Running sequential...");
+                sw.Restart();
+                var dictSeq = RunSequential(inputFile);
+                sw.Stop();
+                Console.WriteLine(" Done.");
+
+                Console.WriteLine("Sequential time: {0:F6}", (sw.ElapsedMilliseconds / 1000.0));
+
+                Console.WriteLine("Parallel results:");
+                print(dictPara);
+                Console.WriteLine("");
+                Console.WriteLine("Sequential results:");
+                print(dictSeq);
+
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        private void print(ConcurrentDictionary<string, int> dict)
+        {
+            foreach (var item in dict)
+            {
+                Console.WriteLine("Word: {0} Count: {1}", item.Key, item.Value);
+            }
+        }
+
+        private ConcurrentDictionary<string, int> RunSequential(string file)
+        {
+            var fileStream = new FileStream(file, FileMode.Open);
             string text = "";
             using (StreamReader reader = new StreamReader(fileStream))
             {
                 text = reader.ReadToEnd();
             }
-
-            var dict = Run(text);
-            foreach (var item in dict)
-            {
-                Console.WriteLine("Word: {0} Count: {1}", item.Key, item.Value);
-            }
-            Console.ReadLine();
+            return CountWords(text, new ConcurrentDictionary<string, int>());
         }
 
-        private ConcurrentDictionary<string, int> Run(string text)
+        private ConcurrentDictionary<string, int> RunParallel(string file)
         {
             var dict = new ConcurrentDictionary<string, int>();
+            Parallel.ForEach(
+                File.ReadLines(file), 
+                new ParallelOptions { }, 
+                () => new ConcurrentDictionary<string, int>(),
+                (line, state, lineNumber, tempDict) =>
+            {
+                CountWords(line, tempDict);
+                return tempDict;
+            },
+                tempDict =>
+                {
+                    foreach (var item in tempDict)
+                    {
+                            if (dict.ContainsKey(item.Key))
+                        {
+                            dict[item.Key] += item.Value;
+                        }
+                            else
+                        {
+                            dict[item.Key] = item.Value;
+                        }
 
+                    }
+                });
+            return dict;
+        }
+
+        private ConcurrentDictionary<string, int> CountWords(string text, ConcurrentDictionary<string, int> dict)
+        {
             text = text.Replace("\n", " ");
             text = text.Replace("\r", "");
             text = text.Replace("\t", " ");
-            var words = text.Split(" ");
+            var words = text.Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
             foreach (var item in words)
             {
-                dict.AddOrUpdate(item, 1, (id, count) => count + 1);
+                if (dict.ContainsKey(item))
+                {
+                    dict[item] += 1;
+                }
+                else
+                {
+                    dict[item] = 1;
+                }
             }
-
-
 
             return dict;
         }
